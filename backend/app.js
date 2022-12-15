3069b7f2e29163310ae8199433f0957318252cf4
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-12-15 17:24:38 -0500

    errors

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -41,5 +42,42 @@ app.use(
 );
 
 app.use(routes);
+
+/* Error handling middleware section */
+
+
+/* Generic resource not found (404) error
+   Note that this only generates an error,
+   and does not accept incoming errors. */
+app.use((_req, _res, next) => {
+    const err = new Error('The requested resource couldn\'t be found');
+    err.title = 'Resource not found';
+    err.errors = ['The requested resource couldn\'t be found'];
+    err.status = 404;
+    next(err);
+});
+
+/* Sequelize error, note that this both
+   takes in and sends an error */
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err)
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
 
 module.exports = app;
