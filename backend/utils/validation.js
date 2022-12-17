f343c7f4f023e858df97704410286809c416f531
Noraa Stoke <noraa.july.stoke@gmail.com>
2022-12-16 22:09:01 -0500

    validation error function -wip

---
+++ backend/utils/validation.js
@@ -0,0 +1,30 @@
+//----------------------------------------------------------
+//----------------| UTILS/VALIDATION.JS |-------------------
+//----------------------------------------------------------
+
+//|Imports & Variable Declarations| ------------------------
+
+const { validationResult } = require('express-validator');
+
+
+// |Validates request bodies for auth routes| --------------
+
+const handleValidationErrors = (req, _res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()){
+        const errors = validationErrors
+            .array()
+            .map((error) => {`${error.msg}`});
+
+            const err = Error('Bad request');
+            err.errors = errors;
+            err.status = 400;
+            err.title = 'Bad request.';
+            next(err);
+    }
+    next();
+}
+
+
+module.exports = { handleValidationErrors };
